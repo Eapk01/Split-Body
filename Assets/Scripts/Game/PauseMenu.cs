975c03cfb0ac41269e8123d1ce5b511ccac7f166
weaamalmuhammadi <weaamalmuhammadi@gmail.com>
2026-05-15 22:30:04 +0300

    Add pause menu, level transition and GUI

---
+++ Assets/Scripts/Game/PauseMenu.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool IsPaused = false;
+
+    [SerializeField] GameObject pauseMenuUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) ||
+            UnityEngine.InputSystem.Gamepad.current != null &&
+            UnityEngine.InputSystem.Gamepad.current.startButton.wasPressedThisFrame)
+        {
+            if (IsPaused) Resume();
+            else Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        IsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        IsPaused = true;
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1f;
+        IsPaused = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Quit()
+    {
+        Time.timeScale = 1f;
+        IsPaused = false;
+        Application.Quit();
+    }
+}
